7697b7fbb0e367065f31adbc9ed3cd8ad3c062ab
emergencyx <emergencyx@gmail.com>
2025-02-28 16:56:39 -0500

    Updated server with rate limiting and token gating fixes working version

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080; // Railway assigns PORT automatically
 
-const TOKEN_MINT = "8JRYGtJ3DueTTQvhD8atiDgycCcXYQq76f6rzK5Hpump"; // CyberFox Token Mint
+const TOKEN_MINT = "CyFoxoQhcjJFQJtz32SaYZFUfs1xLMXoXz3nBSytGvcD"; // CyberFox Token Mint
 const MIN_REQUIRED_TOKENS = 750000;
 
 app.use(cors());
